f66bd3a08c5fef3dfbf9881c27071acc6739dcfe
Adam Biśta <adamosekk2002@gmail.com>
2023-07-25 22:35:53 +0200

    feat: add ProductModal for detailed product information
    Added a new ProductModal component which displays detailed information about a product. The initial design and styling for the component have also been implemented.

--- client/src/components/AfterSearch/ProductCard.js
+++ client/src/components/AfterSearch/ProductCard.js
@@ -2,9 +2,20 @@ import React from 'react';
 import classes from './AfterSearch.module.css';
 import { FaTag, FaStore, FaMapMarkerAlt, FaInfoCircle, FaShoppingCart, FaPlus } from 'react-icons/fa';
 import Button from '../../UI/Button/Button'
+import ProductModal from './ProductModal';
 const MAX_LENGTH = 20; 
 
 const ProductCard = ({ product }) => {
+    const [showModal, setShowModal] = React.useState(false);
+    
+    const openModal = () => {
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
+
     const formatString = (str) => {
         if (str.length > MAX_LENGTH) {
             return str.slice(0, MAX_LENGTH) + "...";
@@ -13,19 +24,21 @@ const ProductCard = ({ product }) => {
     };
     
     return (
-        <div className={classes.productCard}>
+        <React.Fragment>
+        {showModal &&<ProductModal onClose={closeModal} data={product} />}
+        <div className={classes.productCard} onClick={openModal}>
             <h2 className={classes.productTitle}><FaTag className={classes.icon} /> {formatString(product.product_name)}</h2> 
             <p className={classes.productDescription}>{formatString(product.product_description)}</p>
             <p className={classes.productPrice}>Cena: {product.product_price}</p>
             <p className={classes.shopName}><FaStore className={classes.icon} />{formatString(product.shop_name)}</p> 
             <p className={classes.shopAddress}><FaMapMarkerAlt className={classes.icon} />{formatString(product.shop_address)}</p>
-            <Button className={classes.detailButton}><FaInfoCircle className={classes['button-icon']} />Show details</Button>
+            <Button className={classes.detailButton} onClick={openModal}><FaInfoCircle className={classes['button-icon']} />Show details</Button>
             <div className={classes.buttonContainer}>
-                
                 <Button className={classes.cartButton}><FaShoppingCart className={classes['button-icon']} /><FaPlus className={classes['button-icon']} /></Button>
                 <Button className={classes.buyButton}>Buy!</Button>
             </div>
         </div>
+       </React.Fragment>
     );
 };
 
