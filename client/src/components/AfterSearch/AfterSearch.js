e024f643fc1060082bc6e6eae48dc37caf313fa7
Adam Biśta <adamosekk2002@gmail.com>
2023-07-30 14:50:59 +0200

    feat(AfterSearch): add filtering and search count functionality and CSS styling
    Enhanced the AfterSearch component by adding a filtering functionality to streamline the search results. Also added a feature to show the number of searches made, giving users insights into their search patterns. Added CSS styling to improve the visual aesthetics and usability of the component.

--- client/src/components/AfterSearch/AfterSearch.js
+++ client/src/components/AfterSearch/AfterSearch.js
@@ -1,4 +1,6 @@
-import React from 'react';
+
+
+import React,{useState,useEffect} from 'react';
 import ProductCard from './ProductCard';
 import classes from './AfterSearch.module.css';
 
@@ -60,12 +62,75 @@ const dummyData = [
 ];
 
 const AfterSearch = () => {
+    const [products, setProducts] = useState(dummyData);
+    const [filteredProducts, setFilteredProducts] = useState(dummyData);
+    const [filter, setFilter] = useState({ 
+        product_name: '', 
+        shop_name: '', 
+        shop_address: '', 
+        shop_rate: 0, 
+        price: { min: 0, max: 0 } 
+    });
+
+    useEffect(() => {
+        // znajdujemy najniższą i najwyższą cenę wśród produktów
+        const minPrice = Math.min(...products.map(p => Number(p.product_price)));
+        const maxPrice = Math.max(...products.map(p => Number(p.product_price)));
+        // ustawiamy te wartości jako domyślne wartości filtrów ceny
+        setFilter(filter => ({ ...filter, price: { min: minPrice, max: maxPrice } }));
+    }, [products]);
+
+    const filterResults = () => {
+        const results = products.filter(product => 
+            product.product_name.includes(filter.product_name) &&
+            product.shop_name.includes(filter.shop_name) &&
+            product.shop_address.includes(filter.shop_address) &&
+            product.shop_rate >= filter.shop_rate &&
+            Number(product.product_price) >= filter.price.min &&
+            Number(product.product_price) <= filter.price.max
+        );
+        setFilteredProducts(results);
+    };
+
+    useEffect(() => {
+        const identifier = setTimeout(() => {  
+            filterResults();
+        }, 500);
+        return ()=>{clearTimeout(identifier);}
+        
+    }, [filter]);
+
     return (
-        <div className={classes.productList}>
-            {dummyData.map(product => (
-                <ProductCard key={product.product_id} product={product} />
-            ))}
+    <div className={classes.productList}>
+    <div className={classes.count}>Total products found: {filteredProducts.length}</div>
+    <div className={classes.filters}>
+        <div className={classes.row}>
+        Filters:
         </div>
+        <div className={classes.row}>
+        <div className={classes.column}>
+        <input type="text" placeholder="Product Name" onChange={(e) => setFilter({ ...filter, product_name: e.target.value })} />
+        <input type="text" placeholder="Shop Name" onChange={(e) => setFilter({ ...filter, shop_name: e.target.value })} />
+        </div>
+        <div className={classes.column}>
+        <input type="text" placeholder="Shop Address" onChange={(e) => setFilter({ ...filter, shop_address: e.target.value })} />
+        <input type="number" placeholder="Shop Rate (Min)" onChange={(e) => setFilter({ ...filter, shop_rate: e.target.value })} />
+        </div>
+        </div>
+        <div className={classes.row}>
+            <label htmlFor="Price_From">Price From</label>
+        <input id="Price_From" type="number" placeholder="Price From" value={filter.price.min} onChange={(e) => setFilter({ ...filter, price: { ...filter.price, min: e.target.value } })} />
+        <label  htmlFor="Price_To">To</label>
+        <input id="Price_To" type="number" placeholder="Price To" value={filter.price.max} onChange={(e) => setFilter({ ...filter, price: { ...filter.price, max: e.target.value } })} />
+        </div>
+    </div>
+    <div className={classes.results}>
+        {filteredProducts.map(product => (
+            <ProductCard key={product.product_id} product={product} />
+        ))}
+    </div>
+</div>
+
     );
 };
 
