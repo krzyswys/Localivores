e73794fc80344dd593dfbcc6ceba184cc1b7e447
Adam Biśta <adamosekk2002@gmail.com>
2023-08-12 19:43:33 +0200

    feat(AuthForm): enhance password fields and validation
    - Add visibility toggle icon for password fields to preview the password.
    - Introduce 'confirm password' field for Signup form.
    - Implement advanced field validation for password input.

--- client/src/components/AuthenticationPage/AuthForm.js
+++ client/src/components/AuthenticationPage/AuthForm.js
@@ -1,27 +1,30 @@
-import React, { useState } from 'react';
-import './AuthForm.scss'; // We will create this CSS file in the next step.
-import Button from '../../UI/Button/Button'
-import Logo from '../../UI/Logo/Logo'
-import classes from './FormLogo.module.css'
-import {useNavigate} from 'react-router-dom';
-import useInput from './use-input'
+import React, { useState } from "react";
+import "./AuthForm.scss"; // We will create this CSS file in the next step.
+import Button from "../../UI/Button/Button";
+import Logo from "../../UI/Logo/Logo";
+import classes from "./FormLogo.module.css";
+import { useNavigate } from "react-router-dom";
+import useInput from "./use-input";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(false);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
   const navigate = useNavigate();
 
   const navigateHandler = () => {
-    navigate('/after');
+    navigate("/after");
   };
 
   const {
     value: enteredName,
     hasError: nameHasError,
-    isValid: nameIsValud,
+    isValid: nameIsValid,
     valueChangeHandler: nameChangeHandler,
     inputBlurHandler: nameBlurHandler,
     reset: nameReset,
-  } = useInput((value) => value.trim() !== '');
+  } = useInput((value) => value.trim().length >= 3);
 
   const {
     value: enteredEmail,
@@ -30,7 +33,11 @@ const AuthForm = () => {
     valueChangeHandler: emailChangeHandler,
     inputBlurHandler: emailBlurHandler,
     reset: emailReset,
-  } = useInput((value) => value.includes('@'));
+  } = useInput((value) =>
+    /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(value)
+  );
+
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&#]).+$/;
 
   const {
     value: enteredPassword,
@@ -39,92 +46,177 @@ const AuthForm = () => {
     valueChangeHandler: passwordChangeHandler,
     inputBlurHandler: passwordBlurHandler,
     reset: passwordReset,
-  } = useInput((value) => value.trim() !== '');
+  } = useInput((value) => passwordRegex.test(value));
+
+  const {
+    value: enteredConfirmPassword,
+    hasError: confirmPasswordHasError,
+    isValid: confirmPasswordIsValid,
+    valueChangeHandler: confirmPasswordChangeHandler,
+    inputBlurHandler: confirmPasswordBlurHandler,
+    reset: confirmPasswordReset,
+  } = useInput((value) => value === enteredPassword);
 
   let loginIsValid = false;
   let signupIsValid = false;
-  if (nameIsValud && emailIsValid && passwordIsValid) {
+  if (
+    nameIsValid &&
+    emailIsValid &&
+    passwordIsValid &&
+    confirmPasswordIsValid
+    
+  ) {
     signupIsValid = true;
   }
+
   if (emailIsValid && passwordIsValid) {
     loginIsValid = true;
   }
 
-
-  const changeAuth = () =>{
+  const changeAuth = () => {
     nameReset();
     emailReset();
     passwordReset();
+    confirmPasswordReset();
     setIsLogin((prevState) => !prevState);
-    
-  }
+  };
 
   return (
     <main className="auth-form">
       <Logo className={classes.logo} onClick={navigateHandler} />
       <div className="form-structor">
         <div className={isLogin ? "signup slide-up" : "signup"}>
-          <h2 className="form-title" id="signup" onClick={changeAuth}><span>or</span>Sign up</h2>
-          {!isLogin && <div className="form-holder">
+          <h2 className="form-title" id="signup" onClick={changeAuth}>
+            <span>or</span>Sign up
+          </h2>
+          <div className="form-holder">
             <input
               type="text"
-              className={`input ${nameHasError ? 'invalid' : ''}`}
+              className={`input ${nameHasError ? "invalid" : ""}`}
               placeholder="Name"
               name="name"
+              id="signup_name"
               value={enteredName}
               onChange={nameChangeHandler}
               onBlur={nameBlurHandler}
             />
-            {nameHasError && <p className="error-text">Please enter a valid name.</p>}
+            {nameHasError && (
+              <label htmlFor="signup_name" className="error-text">The name must be at least 3 characters long.</label>
+            )}
             <input
               type="email"
-              className={`input ${emailHasError ? 'invalid' : ''}`}
+              className={`input ${emailHasError ? "invalid" : ""}`}
               placeholder="Email"
               name="email"
+              id="signup_email"
               value={enteredEmail}
               onChange={emailChangeHandler}
               onBlur={emailBlurHandler}
             />
-            {emailHasError && <p className="error-text">Please enter a valid email.</p>}
-            <input
-              type="password"
-              className={`input ${passwordHasError ? 'invalid' : ''}`}
-              placeholder="Password"
-              name="password"
-              value={enteredPassword}
-              onChange={passwordChangeHandler}
-              onBlur={passwordBlurHandler}
-            />
-            {passwordHasError && <p className="error-text">Please enter a valid password.</p>}
-          </div>}
-          <Button type="submit" className="submit-btn" disabled={!signupIsValid}>Sign up</Button>
-        </div>
-        <div className={isLogin ? "login" : "login slide-up"}>
-          <div className="center">
-            <h2 className="form-title" id="login" onClick={changeAuth}><span>or</span>Log in</h2>
-            {isLogin && <div className="form-holder">
+            {emailHasError && (
+              <label htmlFor="signup_email" className="error-text">Please enter a valid email.</label>
+            )}
+            <div className="password-container">
               <input
-                type="email"
-                className={`input ${emailHasError ? 'invalid' : ''}`}
-                placeholder="Email"
-                name="email"
-                value={enteredEmail}
-                onChange={emailChangeHandler}
-                onBlur={emailBlurHandler}
-              />
-              {emailHasError && <p className="error-text">Please enter a valid email.</p>}
-              <input
-                type="password"
-                className={`input ${passwordHasError ? 'invalid' : ''}`}
+                type={isPasswordVisible ? "text" : "password"}
+                className={`input ${passwordHasError ? "invalid" : ""}`}
                 placeholder="Password"
                 name="password"
+                id="signup_password"
                 value={enteredPassword}
                 onChange={passwordChangeHandler}
                 onBlur={passwordBlurHandler}
               />
-              {passwordHasError && <p className="error-text">Please enter a valid password.</p>}
-            </div>}
-            <Button type="submit" className="submit-btn" disabled={!loginIsValid}>Log in</Button>
+              <button
+                onClick={() => setIsPasswordVisible((prev) => !prev)}
+                className="visibility-icon"
+                aria-label={
+                  isPasswordVisible ? "Hide password" : "Show password"
+                }
+              >
+                {isPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+
+            {passwordHasError && (
+              <label htmlFor="signup_password" className="error-text">
+                Password must have lowercase and uppercase letters and at least
+                one special character.
+              </label>
+            )}
+            <input
+              type={isPasswordVisible ? "text" : "password"}
+              className={`input ${confirmPasswordHasError ? "invalid" : ""}`}
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              id="signup_confirmPassword"
+              value={enteredConfirmPassword}
+              onChange={confirmPasswordChangeHandler}
+              onBlur={confirmPasswordBlurHandler}
+            />
+            {confirmPasswordHasError && (
+              <label htmlFor="signup_confirmPassword" className="error-text">Passwords don't match.</label>
+            )}
+          </div>
+          <Button
+            type="submit"
+            className="submit-btn"
+            disabled={!signupIsValid}
+          >
+            Sign up
+          </Button>
+        </div>
+        <div className={isLogin ? "login" : "login slide-up"}>
+          <div className="center">
+            <h2 className="form-title" id="login" onClick={changeAuth}>
+              <span>or</span>Log in
+            </h2>
+            <div className="form-holder">
+              <input
+                type="email"
+                className={`input ${emailHasError ? "invalid" : ""}`}
+                placeholder="Email"
+                name="email"
+                id="login_email"
+                value={enteredEmail}
+                onChange={emailChangeHandler}
+                onBlur={emailBlurHandler}
+              />
+              {emailHasError && (
+                <label htmlFor="login_email" className="error-text">Please enter a valid email.</label>
+              )}
+              <div className="password-container">
+              <input
+                type={isPasswordVisible ? "text" : "password"}
+                className={`input ${passwordHasError ? "invalid" : ""}`}
+                placeholder="Password"
+                name="password"
+                id="login_password"
+                value={enteredPassword}
+                onChange={passwordChangeHandler}
+                onBlur={passwordBlurHandler}
+              />
+              <button
+                onClick={() => setIsPasswordVisible((prev) => !prev)}
+                className="visibility-icon"
+                aria-label={
+                  isPasswordVisible ? "Hide password" : "Show password"
+                }
+              >
+                {isPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+              {passwordHasError && (
+                <label htmlFor="login_password" className="error-text">Please enter a valid password.</label>
+              )}
+            </div>
+            <Button
+              type="submit"
+              className="submit-btn"
+              disabled={!loginIsValid}
+            >
+              Log in
+            </Button>
           </div>
         </div>
       </div>
