106ea34ca7e0342d4766ab69b46d967745780be5
bartipablo <bartek.pawlowski00@gmail.com>
2023-08-24 23:41:50 +0200

    fix(database) correct OrdersDTO

--- server/src/main/java/server/dto/OrdersDto.java
+++ server/src/main/java/server/dto/OrdersDto.java
@@ -1,2 +1,22 @@
-package server.dto;public class OrdersDto {
+package server.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrdersDto {
+    private Long id;
+    private Long paymentId;
+    private List<Long> orderItemIds;
+    private Date orderDate;
+    private Date deliveryDate;
+    private String orderStatus;
+    private BigDecimal deliveryCost;
+    private Long driverId;
 }
