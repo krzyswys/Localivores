279fb5183c910613365d7d56e6ed2b7bace2a714
Adam Biśta <adamosekk2002@gmail.com>
2023-08-31 00:59:31 +0200

    styles: styles for confirm locations

--- client/src/components/CheckoutPage/CheckoutInput.js
+++ client/src/components/CheckoutPage/CheckoutInput.js
@@ -3,15 +3,32 @@ import React from 'react';
 import styles from './CheckoutInput.module.css';
 
 const CheckoutInput = (props) => {
-    //connect props classes with this component classes
     const classes = `${styles.inputBox} ${props.className}`;
+    let inputElement;
+
+    switch (props.type) {
+        case 'select':
+            inputElement = (
+              <select >
+                  {props.options.map(opt => <option key={opt.value} value={opt.value}>{opt.label}</option>)}
+              </select>
+            );
+            break;
+        case 'textarea':
+            inputElement = <textarea  />;
+            break;
+        default:
+            inputElement = <input type="text"  />;
+    }
+
   return (
     <div className={classes}>
-      <input required type="text"  />
+      {inputElement}
       <span>{props.label || ""}</span>
     </div>
   );
 };
+
 
 export default CheckoutInput;
 
--- client/src/components/CheckoutPage/ConfirmLocation/AddressForm/AddressForm.js
+++ client/src/components/CheckoutPage/ConfirmLocation/AddressForm/AddressForm.js
@@ -3,11 +3,16 @@ import classes from "./AddressForm.module.css";
 import CheckoutInput from "../../CheckoutInput";  // Załóżmy, że ścieżka jest właściwa
 
 const AddressForm = () => {
+  const deliveryOptions = [
+    { label: "To Door", value: "door" },
+    { label: "Pick Up Outside", value: "outside" }
+  ];
+  
   return (
     <div className={classes["address-form-container"]}>
       <form>
           <CheckoutInput label="Address" />
-        <label>
+        {/* <label>
           Delivery Type:
           <select className={classes.input}>
             <option value="door">To Door</option>
@@ -17,7 +22,10 @@ const AddressForm = () => {
         <label>
           Delivery Note:
           <textarea className={classes.input} name="note" />
-        </label>
+        </label> */}
+        
+          <CheckoutInput label="Delivery Type" type="select" options={deliveryOptions} className={classes.input} />
+          <CheckoutInput label="Delivery Note" type="textarea" className={classes.input} />
       </form>
     </div>
   );
